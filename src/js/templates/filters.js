063bf692f211c84aef03066400883c6ce51057df
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-11-22 19:50:18 +0200

    create category js

--- src/js/templates/filters.js
+++ src/js/templates/filters.js
@@ -1,9 +1,26 @@
 import { ProductAPI } from '../products/API';
 
-const productAPI = new ProductAPI();
-
-// console.log(productAPI.getProducts());
-
 document.addEventListener('DOMContentLoaded', onDocumentLoad);
+const refs = {
+  selectEl: document.querySelector('.category-choice'),
+};
 
-// function onDocumentLoad
+const productAPI = new ProductAPI();
+console.log(productAPI.getProducts());
+function onDocumentLoad() {
+  productAPI.getProducts().then(res => {
+    renderOption(res);
+  });
+}
+
+function createOption(arr) {
+  return arr.map(el => {
+    return `
+        <option value="${el}">${el}</option>
+        `;
+  });
+}
+function renderOption(arr) {
+  const markup = createOption(arr).join('');
+  refs.selectEl.innerHTML = markup;
+}

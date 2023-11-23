e9d94ff9de411a868ff3667d62b3c6ffe2e8b968
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-11-23 17:32:22 +0200

    популяр рендер готово

--- src/js/products/API_popular.js
+++ src/js/products/API_popular.js
@@ -5,12 +5,7 @@ axios.defaults.baseURL = 'https://food-boutique.b.goit.study/api/';
 export class ProductPopularAPI {
   constructor() {}
 
-  getCategories() {
-    return axios.get('products/categories').then(res => res.data);
-  }
-  getProductsByCat(cat) {
-    return axios
-      .get(`products?keyword=Ac&category=${cat}&page=1&limit=9`)
-      .then(res => res.data);
+  getPopular() {
+    return axios.get('products/popular').then(res => res.data);
   }
 }

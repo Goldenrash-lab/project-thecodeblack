ec018af6a0ba431686b85e7cdbd512f9b701125d
VictorFil67 <victorfilinovych@gmail.com>
2023-11-24 00:37:28 +0200

    Base localStorage

--- src/js/templates/localStorage.js
+++ src/js/templates/localStorage.js
@@ -1,8 +1,24 @@
 const productsListCart = document.querySelector('.products__list');
-console.log(productsListCart);
+
 productsListCart .addEventListener('click', onProductsListCartClick);
+
 function onProductsListCartClick(e) {
-    console.log(e.target.parentNode.parentNode.getAttribute('[data-id]'));
-    // console.log(e.target.parentNode);
-    // localStorage.setItem();
+    
+    if(e.target.nodeName !== "path" && e.target.nodeName !== "svg" && e.target.nodeName !== "BUTTON"){
+        return
+    }
+    const id = e.target.closest('.products__item').dataset.id;
+    const btn = e.target.closest('.products__item-link');
+
+    const localStorageItem = localStorage.getItem("cartIds");
+    if (!localStorageItem) {
+        localStorage.setItem("cartIds", JSON.stringify([id]));
+    } else if (JSON.parse(localStorage.getItem("cartIds")).includes(id)) {
+btn.innerHTML = '<p>yes</p>'
+    } else {
+        const ids = JSON.parse(localStorage.getItem("cartIds"));
+        ids.push(id);
+        localStorage.setItem("cartIds", JSON.stringify(ids));
+    }
+    
 }

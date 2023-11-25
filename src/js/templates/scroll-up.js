69f9e34ed2ccf74459a985e18fcef942c537736c
Andriy Razumnykh <127974278+Andreykeyr@users.noreply.github.com>
2023-11-25 21:16:16 +0200

    icon update+
    Делаем проверку на ширину экрана устройства, если меньше 768пх - отрубаем кнопку

--- src/js/templates/scroll-up.js
+++ src/js/templates/scroll-up.js
@@ -1,16 +1,27 @@
-window.addEventListener('scroll', function() {
-   let scrollButton = scrollBtn;
-   if (window.scrollY > window.innerHeight / 2) {
-     scrollButton.style.display = 'block';
-   } else {
-     scrollButton.style.display = 'none';
-   }
- });
-const scrollBtn = document.getElementById('scrollBtn')
+function checkScreenWidth(mediaQuery) {
+  let scrollButton = document.getElementById('scrollBtn');
+  if (mediaQuery.matches) {
+    scrollButton.style.display = 'none';
+  } else {
+    scrollButton.style.display = 'block';
+  }
+}
 
- scrollBtn.addEventListener('click', function() {
+let mediaQuery = window.matchMedia('(max-width: 767px)');
+checkScreenWidth(mediaQuery);
+
+window.addEventListener('scroll', function() {
+  let scrollButton = document.getElementById('scrollBtn');
+  if (window.scrollY > window.innerHeight / 2 && !mediaQuery.matches) {
+    scrollButton.style.display = 'block';
+  } else {
+    scrollButton.style.display = 'none';
+  }
+});
+
+ document.getElementById('scrollBtn').addEventListener('click', function() {
    window.scrollTo({
      top: 0,
-     behavior: 'smooth'
+     behavior: 'smooth',
    });
  });
